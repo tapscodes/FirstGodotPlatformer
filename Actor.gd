a9a37fe81d200e2d8adc158e9576f143e12d3745
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2020-01-27 13:13:35 -0500

    Added Basic TileMap

---
+++ Actor.gd
@@ -0,0 +1,10 @@
+extends KinematicBody2D
+class_name Actor
+var velocity: = Vector2.ZERO
+export var speed = Vector2(300, 1000)
+export var gravity: = 3000.0
+#processes anything relating to physics
+func _physics_process(delta: float) -> void:
+	#delta to make it frame-rate independant
+	velocity.y += gravity * delta
+	velocity = move_and_slide(velocity)
