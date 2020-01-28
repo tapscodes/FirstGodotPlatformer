154d4e8128b915bbaeded90b04358c348c124d39
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2020-01-27 23:19:50 -0500

    Added Gravity to Enemy

--- src/Actors/Enemy.gd
+++ src/Actors/Enemy.gd
@@ -4,6 +4,7 @@ func _ready():
 	_velocity.x = -speed.x #moves left at the "speed" defined in the inspector
 #physics handling function
 func _physics_process(delta: float) -> void:
+	_velocity.y += gravity * delta
 	if (is_on_wall()):
 		_velocity.x *= -1.0 #turns enemy around if it hits a wall
 	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
