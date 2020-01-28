aff0de1226eb792951b9e48b427bea1cf5e08146
Tristan Pudell-Spatscheck <44383662+tapscodes@users.noreply.github.com>
2020-01-27 22:46:05 -0500

    Added Working Jump

--- Actor.gd
+++ Actor.gd
@@ -1,6 +1,7 @@
 extends KinematicBody2D
 class_name Actor
 var velocity: = Vector2.ZERO
+const FLOOR_NORMAL: = Vector2.UP #Used to help move_and_slide properly do gravity
 export var speed = Vector2(300, 1000)
 export var gravity: = 3000.0
 #processes anything relating to physics
--- src/Actors/Player.gd
+++ src/Actors/Player.gd
@@ -4,8 +4,7 @@ func _physics_process(delta: float) -> void:
 	var direction = get_direction()
 	velocity = calculate_velocity(velocity, direction, speed)
 	#Moves Character
-	velocity = move_and_slide(velocity)
-#Gets the direction based on inputs
+	velocity = move_and_slide(velocity, FLOOR_NORMAL) 
 func get_direction() -> Vector2:
 	var dirX = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
 	var dirY = -1.0 if Input.is_action_just_pressed("jump") and is_on_floor() else 1.0
@@ -17,8 +16,8 @@ direction: Vector2,
 speed: Vector2
 ) -> Vector2:
 	var new_velocity = linear_velocity
-	new_velocity.x = speed.x * get_direction().x
+	new_velocity.x = speed.x * direction.x
 	new_velocity.y += gravity * get_physics_process_delta_time()
-	if (velocity.y == -1.0):
-		velocity.y = speed.y * get_direction().y
-	return speed * direction
+	if (direction.y == -1.0): #if a jump was detected
+		new_velocity.y = speed.y * get_direction().y
+	return new_velocity
